7099669fc500221157b94aba3cc3fb423c0178ae
Inês Pinto <inesgp@ASUS.ines>
2021-03-19 15:56:00 +0000

    values onto rc.m file

---
+++ t1/mat/rc.m
@@ -0,0 +1,21 @@
+close all
+clear all
+
+pkg load symbolic
+
+syms t
+
+R1 = 1.02738610688 %Ohm
+R2 = 2.03394761163 %Ohm
+R3 = 3.13880655715 %Ohm
+R4 = 4.00338881943 %Ohm
+R5 = 3.07163426496 %Ohm
+R6 = 2.05278260557 %Ohm
+R7 = 1.01283324471 %Ohm
+Va = 5.18937402699 
+Id = 1.00970251859 
+Kb = 7.32338601881 
+Kc = 8.32372114868 
+
+printf("mesh analysis")
+%now this is will be ines' attempt to the mesh analysis
