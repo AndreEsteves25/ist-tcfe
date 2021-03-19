612773f7ba7e972d1ea2223abb51eea06d6d4930
Inês Pinto <inesgp@ASUS.ines>
2021-03-19 23:19:14 +0000

    mesh analysis complete, missing : extrapolating results to latex

--- t1/mat/rc.m
+++ t1/mat/rc.m
@@ -2,20 +2,25 @@ close all
 clear all
 
 pkg load symbolic
+pkg load miscellaneous
+syms I1
+syms I2
+syms I3
+syms I4
+R1=vpa(1.02738610688)
+R2=vpa(2.03394761163)
+R3 = vpa(3.13880655715)
+R4 = vpa(4.00338881943)
+R5 = vpa(3.0716342649600000000000000000000)
+R6 = vpa(2.05278260557)
+R7 = vpa(1.01283324471)
+Va = vpa(5.18937402699)
+Id = vpa(1.00970251859)
+Kb=vpa(7.32338601881)
+Kc=vpa(8.32372114868)
+Z = vpa(0.0)
+O=vpa(1.0)
 
-syms t
-
-R1 = 1.02738610688 %Ohm
-R2 = 2.03394761163 %Ohm
-R3 = 3.13880655715 %Ohm
-R4 = 4.00338881943 %Ohm
-R5 = 3.07163426496 %Ohm
-R6 = 2.05278260557 %Ohm
-R7 = 1.01283324471 %Ohm
-Va = 5.18937402699 
-Id = 1.00970251859 
-Kb = 7.32338601881 
-Kc = 8.32372114868 
-
-printf("mesh analysis")
-%now this is will be ines' attempt to the mesh analysis
+A=[-R1-R3+R4 , -R3 , Z , R4 ; -R4 , Z, Z , -R6-R7-Kc-R4 ; Z ,Z , O, Z ; Kb*R3, -O,Z,Z]
+B=[Va;Z;Id;Z]
+A\B
