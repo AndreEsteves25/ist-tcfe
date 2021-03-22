c1828c998e0ecee4f637fe2aa4f6ee3b9dc6512e
Inês Pinto <inesgp@ASUS.ines>
2021-03-22 23:00:36 +0000

    entornei o chax2

--- t1/mat/rc.m
+++ t1/mat/rc.m
@@ -2,6 +2,7 @@ close all
 clear all
 
 pkg load symbolic
+
 syms I1
 syms I2
 syms I3
@@ -23,18 +24,119 @@ O=vpa(1.0)
 A=[R1+R3+R4 , R3 , Z , R4 ; R4 , Z, Z , R6+R7-Kc+R4 ; Z ,Z , O, Z ; Kb*R3, Kb*R3-O,Z,Z]
 B=[Va;Z;Id;Z]
 V=A\B
-filename = 'tabela1.txt'
+filename = 'tabela1.tex'
 fid=fopen(filename,'w')
-fprintf(fid,'I1 & %f ',double(V(1)) *1000);
-fprintf(fid,'\n\n ')
-fprintf(fid,'I2 & %f ',double(V(2))*1000);
-fprintf(fid,'\n\n \')
-fprintf(fid,'hline\n')
-fprintf(fid,'I3 & %f ',double(V(3))*1000);
-fprintf(fid,'\n\n \')
-fprintf(fid,'hline\n')
-fprintf(fid,'I4 & %f ',double(V(4))*1000);
-fprintf(fid,'\n\n \')
-fprintf(fid,'hline\n')
+fid2=fopen('data.tex','w')
+s='\'
+t='hline'
+u=' '
+
+I1 = double(V(1)) *1000
+I2 = double(V(2))*1000
+I3 = double(V(3))*1000
+I4 = double(V(4))*1000
+
+fprintf(fid,'I1 & %f ',I1);
+fprintf(fid,s)
+fprintf(fid,s)
+fprintf(fid,u)
+fprintf(fid,s)
+fprintf(fid,t)
+fprintf(fid,'\n')
+fprintf(fid,'I2 & %f ',I2);
+fprintf(fid,s)
+fprintf(fid,s)
+fprintf(fid,u)
+fprintf(fid,s)
+fprintf(fid,t)
+fprintf(fid,'\n')
+fprintf(fid,'I3 & %f ',I3);
+fprintf(fid,s)
+fprintf(fid,s)
+fprintf(fid,u)
+fprintf(fid,s)
+fprintf(fid,t)
+fprintf(fid,'\n')
+fprintf(fid,'I4 & %f ',I4);
+fprintf(fid,s)
+fprintf(fid,s)
+fprintf(fid,u)
+fprintf(fid,s)
+fprintf(fid,t)
+fprintf(fid,'\n')
+
+IVa = I1
+IR1 = I1
+IR2 = I2
+IR3 = I1+I2
+IR4 = I1+I4
+IR5 = I2-I3
+IR6 = I4
+IR7 = I4
+IVc = I4-I3 
+IId = I3
+
+fprintf(fid2,'Va & %f ',IVa);
+fprintf(fid2,s)
+fprintf(fid2,s)
+fprintf(fid2,u)
+fprintf(fid2,s)
+fprintf(fid2,t)
+fprintf(fid2,'\nR1 & %f ',IR1);
+fprintf(fid2,s)
+fprintf(fid2,s)
+fprintf(fid2,u)
+fprintf(fid2,s)
+fprintf(fid2,t)
+fprintf(fid2,'\nR2 & %f ',IR2);
+fprintf(fid2,s)
+fprintf(fid2,s)
+fprintf(fid2,u)
+fprintf(fid2,s)
+fprintf(fid2,t)
+fprintf(fid2,'\nR3 & %f ',IR3);
+fprintf(fid2,s)
+fprintf(fid2,s)
+fprintf(fid2,u)
+fprintf(fid2,s)
+fprintf(fid2,t)
+fprintf(fid2,'\nR4 & %f ',IR4);
+fprintf(fid2,s)
+fprintf(fid2,s)
+fprintf(fid2,u)
+fprintf(fid2,s)
+fprintf(fid2,t)
+fprintf(fid2,'\nR5 & %f ',IR5);
+fprintf(fid2,s)
+fprintf(fid2,s)
+fprintf(fid2,u)
+fprintf(fid2,s)
+fprintf(fid2,t)
+fprintf(fid2,'\nR6 & %f ',IR6);
+fprintf(fid2,s)
+fprintf(fid2,s)
+fprintf(fid2,u)
+fprintf(fid2,s)
+fprintf(fid2,t)
+fprintf(fid2,'\nR7 & %f ',IR7);
+fprintf(fid2,s)
+fprintf(fid2,s)
+fprintf(fid2,u)
+fprintf(fid2,s)
+fprintf(fid2,t)
+fprintf(fid2,'\nVc & %f ',Vc);
+fprintf(fid2,s)
+fprintf(fid2,s)
+fprintf(fid2,u)
+fprintf(fid2,s)
+fprintf(fid2,t)
+fprintf(fid2,'\nId & %f ',IId);
+fprintf(fid2,s)
+fprintf(fid2,s)
+fprintf(fid2,u)
+fprintf(fid2,s)
+fprintf(fid2,t)
+
 fclose(fid)
+fclose(fid2)
 
