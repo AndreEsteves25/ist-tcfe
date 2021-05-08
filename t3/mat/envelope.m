304cc3fa633fddbda4631d2c34abc7f043547d53
Inês Pinto <inesgp@ASUS.ines>
2021-05-08 17:18:53 +0100

    cenas

--- t3/mat/envelope.m
+++ t3/mat/envelope.m
@@ -10,10 +10,10 @@ w=2*pi*f;
 Vp = 230; %Hz
 
 %chosen variables
-Vs=13.25625; %V
-Ca1=10e-6;   %F
-Rb1=10e3;    %Ohm
-nb_diodes = 23;
+Vs=13.11611; %V
+Ca1=1e-6;   %F
+Rb1=1e3;    %Ohm
+nb_diodes = 25;
 
 
 T=1/2/f; %meio periodo
@@ -23,19 +23,17 @@ VS = Vs*cos(w*t);
 Vbridge = abs(VS);
 
 %calular dentinhos
+vD = 12 /nb_diodes;
+eta=1;
+VT=0.025;
+Is=1e-14;
+rd= eta*VT/(Is* exp( vD/(eta*VT)) )
+Req=nb_diodes*rd + Rb1
+Req*Ca1
+Vripple=Vs*(1-exp(-T/Req/Ca1))
 
-Vripple=Vs*(1-exp(-T/Rb1/Ca1))
-%vO = zeros(1, length(t));
 
 
-filename = 'tabela1.tex'
-fid=fopen(filename,'w')
-
-fprintf(fid,'Vs [V]& %f\\\\ \\hline \n',double(Vs));
-fprintf(fid,'C [F]& %f\\\\ \\hline \n',double(Ca1));
-fprintf(fid,'Rb [$\Omega$]& %f\\\\ \\hline \n',double(Rb1));
-fprintf(fid,'Vripple [V] & %f\\\\ \\hline \n',double(Vripple));
-fclose(fid)
 
 m = Vripple/T;
 figure
@@ -65,40 +63,40 @@ hold
 plot(t,vENV)
 print ("envelope2.eps", "-depsc");
 
-vD = 12 /nb_diodes;
 
-eta=1;
-VT=0.025;
-Is=1e-14;
-rd= eta*VT/(Is* exp( vD/(eta*VT)) )
-Req=nb_diodes*rd/(nb_diodes*rd+Rb1)
 new_ripple=nb_diodes*rd/(nb_diodes*rd+Rb1)*Vripple
 
 t=linspace(0,T,100);
 vF = -new_ripple/T*t;
 
-filename = 'tabela2.tex'
-fid=fopen(filename,'w')
-fprintf(fid,'Vd [V] & %f\\\\ \\hline \n',double(vD));
-fprintf(fid,'Rd [$\Omega$]& %f\\\\ \\hline \n',double(rd));
-fprintf(fid,'Req [$\Omega$]& %f\\\\ \\hline \n',double(Req));
-fprintf(fid,' Previous Vripple [V]& %f\\\\ \\hline \n',double(Vripple));
-fprintf(fid,' Final Vripple [V]& %f\\\\ \\hline \n',double(new_ripple));
-fclose(fid)
+
 
 ripplef = zeros (1,2000);
 VF = zeros (1,2000);
 for i=1:20
   for j=1:100
-    ripplef((i-1)*100+j)=vF(j);
-    VF((i-1)*100+j)=vF(j)+12;
+    ripplef((i-1)*100+j)=vF(j)+new_ripple/2;
+    VF((i-1)*100+j)=vF(j)+12+new_ripple/2;
     endfor
 endfor
 
 
-figure
-hold
+figure;
+hold;
 t=linspace(3,3.2,2000);
 plot(t,ripplef)
 title=("Ripple final")
 print ("final.eps", "-depsc");
+
+cost=Ca1*10^6+Rb1/1000+(nb_diodes+4)*0.1
+merit=1/(cost* (new_ripple+10^(-6) )) %it was assumed that the voltage oscillates around 12V
+
+
+filename = 'tabela2.tex';
+fid=fopen(filename,'w');
+fprintf(fid,'cost & %f\\\\ \\hline \n',cost);
+fprintf(fid,'cost & 12\\\\ \\hline \n');
+fprintf(fid,'deviation & 0\\\\ \\hline \n');
+fprintf(fid,'merit & %f\\\\ \\hline \n',merit);
+fprintf(fid,'ripple & %f\\\\ \\hline \n',ripplef);
+fclose(fid)
